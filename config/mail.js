72529576e71853cd89d9beaa9059c8de08d4eecb
Mansoor Khan <mansoor.khantoru04@gmail.com>
2026-07-11 17:25:15 +0500

    Updated email configuration

--- config/mail.js
+++ config/mail.js
@@ -2,14 +2,16 @@ const nodemailer =require("nodemailer")
 
 const transporter = nodemailer.createTransport({
     service:"gmail",
+    host: 'smtp.gmail.com', // or your SMTP host
+  port: 465,
+  secure: true,
+  family: 4,
     auth:{
         user:process.env.EMAIL,
         pass:process.env.EMAIL_PASS
     }
     
-}
-
-)
+})
 
 
 module.exports = transporter
