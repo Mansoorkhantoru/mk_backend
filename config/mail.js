2ac29b180e1906f787e0a8578418c32c752dab16
Mansoor Khan <mansoor.khantoru04@gmail.com>
2026-07-11 17:42:55 +0500

    hogya

--- config/mail.js
+++ config/mail.js
@@ -1,13 +1,11 @@
-const nodemailer = require("nodemailer")
-
 const transporter = nodemailer.createTransport({
-    host: '74.125.200.108', // Gmail's IPv4 address (current)
-    port: 465,
-    secure: true,
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // STARTTLS
+    family: 4,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASS
     }
 })
-
 module.exports = transporter
