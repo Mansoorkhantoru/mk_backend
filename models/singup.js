12def97708d24f7d3f5becfd8803087bd0c48c30
Mansoor Khan <mansoor.khantoru04@gmail.com>
2026-07-12 15:21:38 +0500

    slug add krdya

--- models/singup.js
+++ models/singup.js
@@ -12,7 +12,13 @@ const UsersingupSchema = mongoose.Schema({
     shopName: {
         type: String,
          default: "", 
-    },
+    },slug: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    sparse: true
+  },
     image: {
         type: String
     },
@@ -35,5 +41,31 @@ const UsersingupSchema = mongoose.Schema({
         type: String
     }
 })
-
+UsersingupSchema.pre('save', async function(next) {
+    // Sirf tab slug generate karein jab shopName change ho ya naya document ho
+    if (this.isModified('shopName') && this.shopName) {
+        // Basic slug generate karein
+        let baseSlug = this.shopName
+            .toLowerCase()
+            .replace(/[^a-z0-9]+/g, '-')
+            .replace(/^-+|-+$/g, '');
+        
+        // Check if slug already exists
+        const existingShop = await mongoose.model('singup').findOne({ slug: baseSlug });
+        
+        if (existingShop && existingShop._id.toString() !== this._id.toString()) {
+            // Agar slug exist karta hai toh unique banayein
+            let counter = 1;
+            let newSlug = `${baseSlug}-${counter}`;
+            while (await mongoose.model('singup').findOne({ slug: newSlug })) {
+                counter++;
+                newSlug = `${baseSlug}-${counter}`;
+            }
+            this.slug = newSlug;
+        } else {
+            this.slug = baseSlug;
+        }
+    }
+    next();
+});
 module.exports = mongoose.model("singup", UsersingupSchema)
